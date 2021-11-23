1ff700a18fb5f869b204addcb719c134176f14a8
Kushagra Ojha <86009160+Krushergod@users.noreply.github.com>
2021-11-23 13:11:06 +0530

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -1,55 +1,28 @@
 import java.util.*; 
-
 public class sort_names { 
-
- 
-
 public static void main(String[] args) { 
-
 String[] x = new String[10]; 
-
 int z; 
-
 String temp; 
-
 Scanner sc = new Scanner(System.in); 
-
-System.out.println("Enter Strings :"); 
-
 for(int i = 0; i<10; i++) { 
-
+System.out.println("Enter Strings :"); 
 x[i] = sc.next(); 
-
 } 
-
 for(int j = 0; j<10; j++) { 
-
 for(int i = 0; i<(9-j); i++ ) { 
-
 z = x[i].compareTo(x[i+1]); 
-
 if(z == 0 || z<0)continue; 
-
 else { 
-
 temp = x[i+1]; 
-
 x[i+1] = x[i]; 
-
 x[i] = temp; 
-
 } 
-
 } 
-
 } 
-
+System.out.println("Printing  the String \n");
 for(int i = 0; i<10; i++){ 
-
-System.out.print(x[i]+" "); 
-
+System.out.print(x[i]+"\n "); 
 }	 
-
 } 
-
 }
